f6589fa76737c99f06362352c24c0cfeeb3d53ad
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-07-02 23:52:52 -0400

    completed CRD except update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // It might be a good idea to add event listener to make sure this file
 // only runs after the DOM has finshed loading.
-
+const ul = document.createElement('ul')
 
 // ------------- LOGIC -------------
 function iterateQuotes(quotes) {
+  document.body.append(ul)
   quotes.forEach(singleQuote)
 }
 
 function singleQuote(quote) {
-  const ul = document.createElement('ul')
   ul.innerHTML += `
   <br>
   <li class='quote-card'>
@@ -16,19 +16,52 @@ function singleQuote(quote) {
       <p class='mb-0'>${quote.quote}</p>
       <footer class="blockquote-footer">${quote.author}</footer>
       <br>
-      <button class='btn-success'>Likes: <span>0</span></button>
-      <button class='btn-danger'>Delete</button>
+      <button data-id=${quote.id} class='btn-success'>Likes: <span>0</span></button>
+      <button data-id=${quote.id} class='btn-danger'>Delete</button>
       </blockquote>
   </li>
   `
-  document.body.append(ul)
+  ul.addEventListener("click", handleClicks)
 }
 
+function handleClicks(event) {
+  if (event.target.className === "btn-danger") {
+    fetchDeleteQuote(event)
+  }
+  if (event.target.className === "btn-success") {
+    fetchCreateLike(event)
+  }
+}
+
+function updateLikesDom(data, event) {
+  const oldLike = parseInt(event.target.children[0].innerText)
+  const newLike = oldLike + 1
+  event.target.children[0].innerText = newLike
+}
+
+// function calculateLikes(quote) {
+//   // debugger
+//    return fetchLikes()
+//    .then(likes => {grabLikes(likes, quote)})
+// }
+//
+// function grabLikes(likes, quote) {
+//   // debugger
+//   const count = likes.filter(like => like.quoteId === quote.id).length
+//   return count
+// }
+
+// function mathLikes(likes, quote) {
+//
+//   const count = likes.filter(like => like.quoteId === quote.id).length
+//   return count
+//   // debugger
+// }
 
 
 // -------- EVENT LISTENERS ---------
 document.addEventListener("DOMContentLoaded", () => {
-  fetchQuotes().then(iterateQuotes)
+  fetchQuotes().then(quotes => {iterateQuotes(quotes, ul)})
 
   const newForm = document.querySelector("#new-quote-form")
   newForm.addEventListener("submit", fetchNew)
@@ -36,13 +69,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // -------- FETCH REQUESTS ---------
 function fetchQuotes() {
-  return fetch("http://localhost:3000/quotes?_embed=likes")
+  return fetch("http://localhost:3000/quotes")
   .then(resp => resp.json())
 }
 
 function fetchNew (event) {
   event.preventDefault()
-  fetch("http://localhost:3000/quotes?_embed=likes", {
+  fetch("http://localhost:3000/quotes", {
     method: "POST",
     headers: {
       "Content-type": "application/json",
@@ -56,3 +89,35 @@ function fetchNew (event) {
   .then(resp => resp.json())
   .then(singleQuote)
 }
+
+function fetchDeleteQuote(event) {
+  // debugger
+  const id = event.target.dataset.id
+  fetch(`http://localhost:3000/quotes/${id}`, {
+    method: "DELETE"
+  })
+  event.path[2].remove()
+}
+
+function fetchCreateLike(event) {
+  const quoteId = parseInt(event.target.dataset.id)
+  fetch("http://localhost:3000/likes", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "quoteId": quoteId,
+      "createdAt": Date.now()
+    })
+  })
+  .then(resp => resp.json())
+  .then(like => {updateLikesDom(like, event)})
+}
+
+// function fetchLikes() {
+//   return fetch("http://localhost:3000/likes")
+//   .then(resp => resp.json())
+//   // .then(likes => {mathLikes(likes, quote)})
+// }
